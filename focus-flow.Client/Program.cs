b417ecd6c9b95022c23c9420ef143b0c4a1ae9e6
MrCrowNFT <mrcrownft@gmail.com>
2025-06-11 10:25:53 -0400

    registered services

--- focus-flow.Client/Program.cs
+++ focus-flow.Client/Program.cs
@@ -6,6 +6,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+//services register
+builder.Services.AddScoped<TaskService>();
+builder.Services.AddScoped<TagService>();
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
